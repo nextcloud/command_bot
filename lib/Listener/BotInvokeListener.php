8a16251e38f2be9590a00e8194df61d628897840
Joas Schilling <coding@schilljs.com>
2025-01-22 17:51:32 +0100

    feat: Add ${sender} and ${1} support like StreamElements
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Listener/BotInvokeListener.php
+++ lib/Listener/BotInvokeListener.php
@@ -47,18 +47,82 @@ class BotInvokeListener implements IEventListener {
 		}
 
 		$content = json_decode($chatMessage['object']['content'], true);
-		if (str_starts_with($content['message'], '!set')
-			 && !in_array((int)$chatMessage['actor']['talkParticipantType'], [
-				self::PARTICIPANT_TYPE_OWNER,
-				self::PARTICIPANT_TYPE_MODERATOR,
-			], true)) {
-			// IF NOT MODERATOR, DO NOT ALLOW "!set …"
-			$this->logger->debug('Can not use !set unless being a moderator');
+		if ($content['message'][0] !== '!' && $content['message'][0] !== '?') {
+			return;
+		}
+
+		[$command, $message] = explode(' ', $content['message'] . ' ', 2);
+		if ($command === '!set') {
+			if (!in_array((int)$chatMessage['actor']['talkParticipantType'], [
+					self::PARTICIPANT_TYPE_OWNER,
+					self::PARTICIPANT_TYPE_MODERATOR,
+				], true)) {
+				// IF NOT MODERATOR, DO NOT ALLOW "!set …"
+				$this->logger->debug('Can not use !set unless being a moderator');
+				return;
+			}
+			$event->addReaction('👍');
 			return;
 		}
 
 		if (str_starts_with($content['message'], '!set')) {
 			$event->addReaction('👍');
 		}
+
+		if ($command === '!hug') {
+			$string = '${sender} shows ${1} some love!';
+
+			$searches = $replacements = [];
+			if (str_contains($string, '${1}')) {
+				$searches[] = '${1}';
+				$mention = $this->getFirstMention($content['parameters']);
+				if ($mention === null) {
+					return;
+				}
+				$replacements[] = $mention;
+			}
+			if (str_contains($string, '${sender}')) {
+				$searches[] = '${sender}';
+				$replacements[] = $this->getSender($chatMessage['actor']);
+			}
+
+			$event->addAnswer(str_replace(
+				$searches,
+				$replacements,
+				$string
+			));
+		}
+	}
+
+	protected function getSender(array $actor): string {
+		[$type, $id] = explode('/', $actor['id'], 2);
+		$type = rtrim($type, 's');
+		if ($type === 'user') {
+			return '@"' . $id . '"';
+		}
+		return '@"' . $type . '/' . $id . '"';
+	}
+
+	protected function getFirstMention(array $parameters): ?string {
+		foreach ($parameters as $parameter) {
+			if ($parameter['type'] === 'call') {
+				return '@all';
+			}
+			if ($parameter['type'] === 'user') {
+				if (isset($parameter['server'])) {
+					return '@"federated_user/' . $parameter['id'] . '@' . $parameter['server'] . '"';
+				} else {
+					return '@"' . $parameter['id'] . '"';
+				}
+			}
+			if ($parameter['type'] === 'user-group') {
+				return '@"group/' . $parameter['id'] . '"';
+			}
+			if ($parameter['type'] === 'guest') {
+				return '@"guest/' . $parameter['id'] . '"';
+			}
+		}
+
+		return null;
 	}
 }
